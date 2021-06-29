1363d42d6d9ba4c40ef198c7f3edfd8bbf7913ff
Jack Do <dominhtrijack@gmail.com>
2021-06-29 23:14:21 +0700

    some more improve

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ var second = document.getElementById("seconds")
 var milisecond = document.getElementById("miliseconds")
 var totalSecond = 0
 var a = 0
+var start = false
 
 function setTime() {
+  second.style.color = "white"
+  milisecond.style.color = "white"
+  minutes.style.color = "white"
+  document.getElementById("semicolon1").style.color = "white"
+  document.getElementById("semicolon2").style.color = "white"
   ++totalSecond
   milisecond.innerHTML = pad(totalSecond % 100)
   second.innerHTML = pad(parseInt(totalSecond / 100) % 60)
@@ -30,11 +36,27 @@ document.body.onkeyup = function (e) {
   if (e.keyCode == 32) {
     a++
     if (a % 2 == 1) {
-      totalSecond = 0
+      reset = 0
       myFunc = setInterval(setTime, 10)
+      totalSecond = 0
     } else if (a % 2 == 0) {
+      reset = 1
       clearInterval(myFunc)
       totalSecond = 0
+    }
+  }
+}
+document.body.onkeydown = function (e) {
+  if (e.keyCode == 32) {
+    second.style.color = "rgb(153, 255, 102)"
+    milisecond.style.color = "rgb(153, 255, 102)"
+    minutes.style.color = "rgb(153, 255, 102)"
+    document.getElementById("semicolon1").style.color = "rgb(153, 255, 102)"
+    document.getElementById("semicolon2").style.color = "rgb(153, 255, 102)"
+    if (reset == 0) {
+      milisecond.innerHTML = "00"
+      second.innerHTML = "00"
+      minutes.innerHTML = "00"
     }
   }
 }
