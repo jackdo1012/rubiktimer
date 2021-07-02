a77904c273383bdd338def280ef85d3b5f53fcd9
Jack Do <dominhtrijack@gmail.com>
2021-07-02 14:53:14 +0700

    add function to timer

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 var minutes = document.getElementById("minutes")
 var second = document.getElementById("seconds")
-var milisecond = document.getElementById("miliseconds")
+var millisecond = document.getElementById("miliseconds")
 var totalSecond = 0
 var a = 0
-var start = false
+window.reset = true
 
 function setTime() {
   second.style.color = "white"
-  milisecond.style.color = "white"
+  millisecond.style.color = "white"
   minutes.style.color = "white"
+  document.getElementById("go").style.display = "none"
   document.getElementById("semicolon1").style.color = "white"
   document.getElementById("semicolon2").style.color = "white"
   ++totalSecond
-  milisecond.innerHTML = pad(totalSecond % 100)
+  millisecond.innerHTML = pad(totalSecond % 100)
   second.innerHTML = pad(parseInt(totalSecond / 100) % 60)
   minutes.innerHTML = pad(parseInt(totalSecond / 6000))
   if (parseInt(totalSecond / 6000) < 1) {
@@ -36,27 +37,30 @@ document.body.onkeyup = function (e) {
   if (e.keyCode == 32) {
     a++
     if (a % 2 == 1) {
-      reset = 0
+      window.reset = false
       myFunc = setInterval(setTime, 10)
       totalSecond = 0
     } else if (a % 2 == 0) {
-      reset = 1
-      clearInterval(myFunc)
+      window.reset = true
       totalSecond = 0
     }
   }
 }
+document.body.onkeypress = function (e) {
+  if (e.keyCode == 32 && window.reset == false) {
+    clearInterval(myFunc)
+  }
+}
 document.body.onkeydown = function (e) {
-  if (e.keyCode == 32) {
+  if (e.keyCode == 32 && window.reset == true) {
     second.style.color = "rgb(153, 255, 102)"
-    milisecond.style.color = "rgb(153, 255, 102)"
+    millisecond.style.color = "rgb(153, 255, 102)"
     minutes.style.color = "rgb(153, 255, 102)"
     document.getElementById("semicolon1").style.color = "rgb(153, 255, 102)"
     document.getElementById("semicolon2").style.color = "rgb(153, 255, 102)"
-    if (reset == 0) {
-      milisecond.innerHTML = "00"
-      second.innerHTML = "00"
-      minutes.innerHTML = "00"
-    }
+    document.getElementById("go").style.display = "inline-block"
+    millisecond.innerHTML = "00"
+    second.innerHTML = "00"
+    minutes.innerHTML = "00"
   }
 }
