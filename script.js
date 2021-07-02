c1317d91ff8ca18458716a770c8cd157b3598ffc
Jack Do <dominhtrijack@gmail.com>
2021-07-02 21:14:43 +0700

    add ao5 and mo5 to timer

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var minutes = document.getElementById("minutes")
 var second = document.getElementById("seconds")
-var millisecond = document.getElementById("miliseconds")
+var millisecond = document.getElementById("milliseconds")
+var mo5 = document.getElementById("meanOfFive")
+var ao5 = document.getElementById("averageOfFive")
 var totalSecond = 0
 var a = 0
 window.reset = true
+var solves = []
+var meanOf5 = 0
 
 function setTime() {
   second.style.color = "white"
@@ -49,6 +53,46 @@ document.body.onkeyup = function (e) {
 document.body.onkeypress = function (e) {
   if (e.keyCode == 32 && window.reset == false) {
     clearInterval(myFunc)
+    fiveSolvesArray = []
+    timeOfSolve =
+      parseInt(totalSecond / 6000) * 60 +
+      (parseInt(totalSecond / 100) % 60) +
+      (totalSecond % 100) / 100
+    solves.push(timeOfSolve)
+    if (solves.length >= 5) {
+      fiveSolvesArray = [
+        solves[solves.length - 1],
+        solves[solves.length - 2],
+        solves[solves.length - 3],
+        solves[solves.length - 4],
+        solves[solves.length - 5],
+      ]
+      meanOf5 = (
+        (fiveSolvesArray[0] +
+          fiveSolvesArray[1] +
+          fiveSolvesArray[2] +
+          fiveSolvesArray[3] +
+          fiveSolvesArray[4]) /
+        5
+      ).toFixed(2)
+      console.log(fiveSolvesArray)
+      fiveSolvesArray.sort(function (a, b) {
+        return a - b
+      })
+      fiveSolvesArray.pop()
+      fiveSolvesArray.shift()
+      averageOf5 = (
+        (fiveSolvesArray[0] + fiveSolvesArray[1] + fiveSolvesArray[2]) /
+        3
+      ).toFixed(2)
+      mo5.style.left = "36vw"
+      ao5.style.left = "36vw"
+    } else {
+      meanOf5 = ""
+      averageOf5 = ""
+    }
+    mo5.innerHTML = "mo5: " + meanOf5
+    ao5.innerHTML = "ao5: " + averageOf5
   }
 }
 document.body.onkeydown = function (e) {
